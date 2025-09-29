fc9b317dd97493aa87f2cd46b9ab8726006ff134
Kaligo812 <gabrielbereciartu14@gmail.com>
2025-09-29 09:44:15 -0300

    simulacro.hs terminado

--- haskell/simulacro.hs
+++ haskell/simulacro.hs
@@ -28,8 +28,42 @@ cantidadNumerosAbundantes d h
     | otherwise = cantidadNumerosAbundantes (d + 1) h
 
 
+
+-- Ejercicio 2
 cursadasVencidas :: [(String, Integer, Integer)] -> [String]
 cursadasVencidas [] = []
 cursadasVencidas ((nombre, anio, cuatri):cursadas)
     | anio < 2021 || (anio == 2021 && cuatri <= 1) = nombre:cursadasVencidas cursadas
     | otherwise = cursadasVencidas cursadas
+
+
+-- Ejercicio 3
+saturarEnUmbralHastaNegativo :: [Integer] -> Integer -> [Integer]
+saturarEnUmbralHastaNegativo [] _ = []
+saturarEnUmbralHastaNegativo (x:xs) u
+    | x < 0 = []
+    | x <= u = x:saturarEnUmbralHastaNegativo xs u
+    | x > u = u:saturarEnUmbralHastaNegativo xs u
+
+
+-- Ejercicio 4
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud (x:xs) = 1 + longitud xs
+
+filtrar :: (t -> Bool) -> [t] -> [t]
+filtrar _ [] = []
+filtrar func (x:xs)
+    | func x = x:filtrar func xs
+    | otherwise = filtrar func xs
+
+iEsimoElemento :: [t] -> Integer -> t
+iEsimoElemento (x:xs) 0 = x
+iEsimoElemento (x:xs) i = iEsimoElemento xs (i - 1)
+
+iEsimaColumna :: [[t]] -> Integer -> [t]
+iEsimaColumna [] _ = []
+iEsimaColumna (xs:xss) i = (iEsimoElemento xs i):iEsimaColumna xss i
+
+cantidadParesColumna :: [[Integer]] -> Integer -> Integer
+cantidadParesColumna xss i = longitud (filtrar (\x -> mod x 2 == 0) (iEsimaColumna xss (i - 1)))
